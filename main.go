c099ec3f156a369fe89687aeeeca0c9751d96731
Gabdylgaziz <g_zhagypar@kbtu.kz>
2024-02-02 01:38:50 +0600

    minor changes

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"discord/internal/controller"
+	"github.com/joho/godotenv"
+	"log"
 	"os"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	discordBot := controller.NewEndpointHandler(os.Getenv("DiscordToken"))
